2cc6b44cb52275fbcda9b3ae8b698ed08dd08b9c
Sunil Shelke <119919644+SharpSunil@users.noreply.github.com>
2026-01-09 22:52:55 +0530

    ss

--- src/componants/Header/Header.jsx
+++ src/componants/Header/Header.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import "./header.scss"
+import logo from "../../assets/logo.png"
+import { CiMenuFries } from "react-icons/ci";
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
+
   return (
     <>
       <div className="header-parent parent">
         <div className="header-cont container">
-            <h1>QR Profile Generator</h1>
+          <div className="logo"><img src={logo} /></div>
+          <div className="menu">
+            <p>Home</p>
+            <p>About</p>
+            <p>Services</p>
+            <p>Contact</p>
+          </div>
+         {open && (
+
+           <div className="mobile-menu">
+            <p>Home</p>
+            <p>About</p>
+            <p>Services</p>
+            <p>Contact</p>
+          </div>
+         )}
+          <div className="toggle" onClick={() => setOpen(!open)}>
+            <CiMenuFries />
+          </div>
         </div>
       </div>
     </>
